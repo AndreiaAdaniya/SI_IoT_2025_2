b230fd7a87f2c56893916644b475cf6b93db2622
AndreiaAdaniya <31291389+AndreiaAdaniya@users.noreply.github.com>
2025-09-27 16:19:50 -0300

    Update WebAppTeste.py

--- Dia_06/StreamlitDashboard/WebAppTeste.py
+++ Dia_06/StreamlitDashboard/WebAppTeste.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.title("Hello Massaki")
+st.write("Opa! Eu sei fazer site!")
+
+# streamlit 30 days - Day 3
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
